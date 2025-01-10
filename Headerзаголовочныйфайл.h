132cd5b250667d69d67d9f9dde664dfc967e9ac5
mariyaborisovait18 <mahaborisovad@gmail.com>
2025-01-10 22:10:10 +0500

    Add files via upload
    Файлы для реализации задачи -  ИКМ.

---
+++ Headerзаголовочныйфайл.h
@@ -0,0 +1,7 @@
+#pragma once
+using namespace std;
+string validate(const string& num);
+string LargeN(string num1, string num2);
+string multN(string num, int dig);
+string LargeNum(string num1, string num2);
+
